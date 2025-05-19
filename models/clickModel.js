671b7f0a2688dbfd36a35909f40aa9867093b4a5
yasmineahmadi <yasminahmadi040@gmail.com>
2025-05-19 23:45:39 +0100

    Update clickModel.js

--- models/clickModel.js
+++ models/clickModel.js
@@ -1,5 +1,5 @@
 const db = require('../config/db');
-
+//db
 const Click = {
   create: (urlId, ipAddress, userAgent, callback) => {
     db.run(`INSERT INTO clicks (url_id, ip_address, user_agent) VALUES (?, ?, ?)`, [urlId, ipAddress, userAgent], callback);
