066933d881eafd13c9ba65dab802c9c52fa231df
yasmineahmadi <yasminahmadi040@gmail.com>
2025-05-19 23:45:22 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const router = express.Router();
-
+//fffff
 router.get('/login', (req, res) => res.render('login', { error: null }));
 router.get('/signup', (req, res) => res.render('signup', { error: null }));
 router.post('/login', authController.login);
